44a2688c03e4e332e701edf4d012993eefcc688e
SHAHZAIB <shahzaibintzar27346@gmail.com>
2024-04-12 17:02:33 +0500

    next auth

---
+++ libs/AuthOptions.tsx
@@ -0,0 +1,39 @@
+import { AuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prismadb from "../libs/prismadb";
+import bcrypt from "bcrypt";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { lable: "Email", type: "email" },
+        passward: { label: "passward", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.passward) {
+          throw new Error("Missing credentials");
+        }
+
+        const user = await prismadb.user.findFirst({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.id || !user.hashedpassword) {
+          throw new Error("invalid credentials");
+        }
+        const currentHashedpassword = await bcrypt.hash(
+          credentials.passward,
+          12
+        );
+
+        if (currentHashedpassword === user.hashedpassword) {
+          throw new Error("invalid credentials");
+        }
+        return uaer;
+      },
+    }),
+  ],
+};
